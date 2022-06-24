ad8d8f9d1b80d02a6563739d91f77431948355ea
AgusLois <Agusslois98@gmail.com>
2022-06-24 10:40:17 -0300

    TP7-Lois

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,16 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import vue from "vue"
+import VueRouter from "vue-router"
 
-Vue.use(VueRouter)
+import Form from '../components/Form.vue'
+import Users from '../components/Users.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+vue.use(VueRouter)
+
+export const router = new VueRouter({
+    node: "history",
+    routes: [
+        {path: "/", redirect: "/form"},
+        {path: "/form", component: Form},
+        {path: "/users", component: Users},
 ]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
 })
-
-export default router
